78303a745dde6653e74b9f4140047a9bf77c93f5
Nader Frigui <nadfri@gmail.com>
2021-11-21 19:21:40 +0100

    Ajout ScrollTop

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,16 +1,34 @@
+'use stricct';
+
 /*ProgressBar*/
 onscroll = () => {
-    const page = document.documentElement; //element HTML
-    let totalHeight = page.scrollHeight; //Height Total of page
-    let visibleHeight = page.clientHeight; //Height visible
-    let scrolling = page.scrollTop; //size of scroll
-    let max = totalHeight - visibleHeight;
-    progressBar.style.width = Math.floor(scrolling / max * 100) + "%"; //width in %
-}
+	const page = document.documentElement; //element HTML
+	let totalHeight = page.scrollHeight; //Height Total of page
+	let visibleHeight = page.clientHeight; //Height visible
+	let scrolling = page.scrollTop; //size of scroll
+	let max = totalHeight - visibleHeight;
+	progressBar.style.width = Math.floor((scrolling / max) * 100) + '%'; //width in %
+};
 
 /*Hamburger*/
 hamburger.onclick = () => {
-    hamburger.classList.toggle("open");
-    // nav_ul.classList.toggle("slide");
-    document.body.classList.toggle("noScroll");
-  };
+	hamburger.classList.toggle('open');
+	// nav_ul.classList.toggle("slide");
+	document.body.classList.toggle('noScroll');
+};
+
+/*Scroll*/
+const handleScroll = () => {
+	if (window.scrollY > 0) {
+		iconScroll.classList.remove('hidden');
+		iconScroll.classList.add('visible');
+	} else iconScroll.classList.replace('visible', 'hidden');
+};
+
+document.addEventListener('scroll', handleScroll);
+
+iconScroll.onclick = () => {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+	iconScroll.classList.add('iconScroll_top');
+	setTimeout(() => iconScroll.classList.remove('iconScroll_top'), 1000);
+};
